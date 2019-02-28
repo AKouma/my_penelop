772765f172efe744d5b8f7c14d37005ca005a8b9
Aboubacar <elbacar08@gmail.com>
2019-02-28 21:42:08 +0100

    [A] in progress

--- src/main/java/Dao/MessageDao.java
+++ src/main/java/Dao/MessageDao.java
@@ -1,15 +1,40 @@
 package Dao;
 
+import Modules.Groupe;
 import Modules.Message;
 import Modules.User;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import static Utils.Constants.messageFilePathName;
+import static Utils.Constants.userFilePathName;
+import static Utils.FileManager.InsertIntoJson;
 
 public class MessageDao implements Idao<Message> {
 
+    private List<Message> messages;
+
     @Override
     public Message create(Message message) {
-        return null;
+        if (message != null) {
+            List<Object> messagesList = new ArrayList<>();
+            messages = findAll();
+            boolean isAlreadyExist = false;
+            Iterator it = messages.iterator();
+            while (it.hasNext() && !isAlreadyExist) {
+                User current = (User) it.next();
+                isAlreadyExist = current.equals(message);
+            }
+            if (!isAlreadyExist) {
+                updateUserMessage(message);
+                messages.add(message);
+                messagesList.addAll(messages);
+                InsertIntoJson(messagesList, messageFilePathName);
+            }
+        }
+        return message;
     }
 
     @Override
@@ -35,5 +60,17 @@ public class MessageDao implements Idao<Message> {
     @Override
     public Message findById(Object id) {
         return null;
+    }
+
+    private void updateUserMessage(Message message){
+        List<User> users = message.getMessageSentTo();
+        users.add(message.getMessageSender());
+        UserDao userDao = new UserDao();
+        for (User user : users) {
+            List<Message> messages = new ArrayList<>();
+            messages.add(message);
+            user.setUserMessages(messages);
+            userDao.update(user);
+        }
     }
 }
